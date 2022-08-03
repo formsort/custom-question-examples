8161d4205116ae4a34a9fb32dbf573b025fe9b9c
Fil Zembowicz <fil@filosophy.org>
2022-08-03 12:23:30 -0400

    Finish up colors for an example custom question

--- zoom-slider/src/routes/index.svelte
+++ zoom-slider/src/routes/index.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import { setAnswerValue, getAnswerValue, clearAnswerValue } from '@formsort/custom-question-api';
 	import { onMount } from 'svelte';
+	import chroma from 'chroma-js';
 
 	const min = 1;
 	const max = 10;
+	const lightColor = '7A9AC4';
+	const darkColor = 'ED7032';
+
 	let selectedValue: number | undefined = undefined;
+
+	const scale = chroma.scale([lightColor, darkColor]);
 
 	onMount(async () => {
 		selectedValue = await getAnswerValue();
@@ -27,10 +33,11 @@
 </script>
 
 <div role="radiogroup">
-	{#each values as value}
+	{#each values as value, idx}
 		<button
 			role="radio"
 			aria-checked="false"
+			style={`color: ${scale(idx / (max - min))}`}
 			class:neighbor={selectedValue != null && Math.abs(selectedValue - value) === 1}
 			class:selected={value === selectedValue}
 			on:click={() => toggleValue(value)}
@@ -52,14 +59,23 @@
 		display: block;
 		width: 120px;
 		height: 50px;
-		transition: font-size 0.1s ease-in-out;
+		transition: all 0.1s ease-in-out;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+		opacity: 0.5;
+		padding: 0;
 	}
 
 	button.selected {
-		font-size: 175%;
+		font-size: 200%;
+		opacity: 1;
+		transform: translateY(7.5%);
 	}
 
 	button.neighbor {
-		font-size: 105%;
+		font-size: 120%;
+		opacity: 0.6;
+		transform: translateY(0.5%);
 	}
 </style>
--- zoom-slider/svelte.config.js
+++ zoom-slider/svelte.config.js
@@ -1,19 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const githubPagesUrl = '/custom-question-examples/zoom-slider/build';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
 		prerender: {
       default: true
     },
 		paths: {
-      base: '/custom-question-examples/zoom-slider/build'
+			base: process.env.NODE_ENV === "production" ? githubPagesUrl : "",
     },
 	}
 };
